41e0723de03ce4cfe0c233dc1e80f83c65fa25e1
rncosta29 <raf.costa29@gmail.com>
2023-07-19 12:25:22 -0300

    Iniciando projeto configurado para JWT

---
+++ src/main/java/br/com/rcosta/autobank/AutobankApplication.java
@@ -0,0 +1,13 @@
+package br.com.rcosta.autobank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutobankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutobankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rcosta/autobank/AutobankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rcosta.autobank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutobankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
